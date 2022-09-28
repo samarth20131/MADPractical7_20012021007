b0100ad779db56c38021445db6d020b8bf5b35e8
samarth20131 <samrajgupta3110@gmail.com>
2022-09-28 23:09:59 +0530

    Second cardview done

---
+++ app/src/main/java/com/example/madpractical7_20012021007/ALarmReceiver.kt
@@ -0,0 +1,13 @@
+package com.example.madpractical7_20012021007
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class ALarmReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("ALarmReceiver.onReceive() is not implemented")
+    }
+}
